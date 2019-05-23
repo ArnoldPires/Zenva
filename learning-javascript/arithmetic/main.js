dd1c5e4f6cde0a0e891624b8c906e4533fccf3ad
ArnoldPires <arnold.pires92@gmail.com>
2019-05-22 21:33:05 -0400

    arithemtic operations

---
+++ learning-javascript/arithmetic/main.js
@@ -0,0 +1,22 @@
+//addition
+var a = 1000;
+var b = a + 200;
+alert(b);
+console.log(b);
+//subtraction
+var c = 100 - 50;
+alert(c);
+console.log(c);
+//multiplication
+var m = c * 10;
+alert(m);
+console.log(m);
+//division
+var d = 500 / 2;
+alert(d);
+console.log(d);
+//mix
+//Note: All math follows PEMDAS
+var combo = (100 + 50) / 2 * 10;
+alert(combo);
+console.log(combo + ' hello there buddy');
