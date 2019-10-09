01008c551a3b54b3dc244f58c9ffae191c493780
Arnold <arnold.pires92@gmail.com>
2019-10-09 13:21:48 -0400

    javascript

---
+++ learning-javascript/begain/main.js
@@ -0,0 +1,19 @@
+//alert function
+alert('Hello world!')
+//Strings
+alert("This is a string")
+//numbers
+alert(8+7*22)
+//How to tell the broswer width
+alert(window.innerWidth)
+alert(window.innerHeight)
+alert(window.scrollY)
+alert(window.origin)
+alert(window.location)
+//Nesting
+//A property of an object can be an object itself
+//Shows the full URL link
+alert(window.location.href)
+//Window is an object, location is an object, href is a string
+//Methods
+alert(window.alert('Thats so cool!'))
